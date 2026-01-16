baf18feca5012a46c0748d2bc53f844b7d3a0153
Soufiane <sokori@sysdos.com>
2026-01-16 17:34:16 +0200

    fixe:collecting test module

--- tests/test_drift.py
+++ tests/test_drift.py
@@ -1,10 +1,10 @@
 from src.compare_data import calculate_drift_logic
 
 def test_drift_detection_math():
-    # Case 1
+    # Case 1: Drift
     score1, drift1 = calculate_drift_logic(10.0, 20.0, 0.2)
     assert drift1 == True
     
-    # Case 2
+    # Case 2: Healthy
     score2, drift2 = calculate_drift_logic(10.0, 11.0, 0.2)
     assert drift2 == False
